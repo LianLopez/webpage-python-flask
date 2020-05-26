203c3f4b2181e3c89b6a47791fcd45ceeb2c47f5
lianlopez <lopez.leonardo.info@gmail.com>
2020-05-26 00:48:14 -0300

    feat: new details, effects and some changes of style

--- index.py
+++ index.py
@@ -2,15 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+help = True
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
-@app.route('/about')
+@app.route('/Sobre-nosotros')
 def about():
     return render_template('about.html')
+
+
+@app.route('/Servicios')
+def services():
+    return render_template('services.html')
 
 
 if __name__ == '__main__':
